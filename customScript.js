f5e9cc5e5f800868294244d517ee8e43838ad1ef
TimShier <tim@timshier.com>
2018-03-17 14:30:17 +0200

    setInterval used to keep it the bad stuff out

--- customScript.js
+++ customScript.js
@@ -1,11 +1,27 @@
-$(document).ready(function() {
-  // update title to let us know we're covered.
-  document.title = 'TW - we got ya!';
+// $(document).ready(function() {
+//   // update title to let us know we're covered.
+//   document.title = 'TW - we got ya!';
+//
+//   $(".tweet").each(function() {
+//     if($(this).text().indexOf("chicken") !== -1){
+//       $( this ).css("background","#FF0000");
+//     }
+//
+//   });
+// });
+//
+// $(".tweet").ready(function() {
+//   if($(this).text().indexOf("chicken") !== -1){
+//     $( this ).css("background","#0000FF");
+//   }
+// });
 
+var tid = setInterval(mycode, 500);
+function mycode() {
   $(".tweet").each(function() {
     if($(this).text().indexOf("chicken") !== -1){
       $( this ).css("background","#FF0000");
     }
 
   });
-});
+}
